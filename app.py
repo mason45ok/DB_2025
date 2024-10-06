167f6b73c4f5c924e59b38facea23db9f8414bf2
Pecu Tsai <pecu610@gmail.com>
2024-10-06 21:59:07 +0800

    UPDATE

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask
-from create import create_bp  # Use create_bp from create.py
+from create import create_bp
 from read import read_bp
+from update import update_bp  # Import the update blueprint
 from delete import delete_bp
 
 app = Flask(__name__)
 
 # Register the blueprints
-app.register_blueprint(create_bp)  # Register create_bp
+app.register_blueprint(create_bp)
 app.register_blueprint(read_bp)
+app.register_blueprint(update_bp)  # Register the update blueprint
 app.register_blueprint(delete_bp)
 
 if __name__ == '__main__':
